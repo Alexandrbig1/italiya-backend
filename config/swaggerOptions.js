086286493a3fc0fb9f358bf37e5ad0c7a06cab28
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-21 20:45:26 -0600

    fix: updated configurations

--- config/swaggerOptions.js
+++ config/swaggerOptions.js
@@ -3,9 +3,9 @@ const swaggerOptions = {
     openapi: "3.0.0",
     info: {
       title: "Italiya's Whisker Wonders API Documentation",
-      description: `\nExplore the API documentation for Italiya's Whisker Wonders website.\n\n🧰 [Backend GitHub repository](https://github.com/Alexandrbig1/italiya-backend)\n\n\n💅
-[Frontend GitHub repository](https://github.com/Alexandrbig1/italiya)\n\n\n🌐
-[Visit Italiya's website](https://alexandrbig1.github.io/italiya/)\n\n\n Step into the enchanting world of Italiya: our charismatic orange cat. Indulge in whisker wonders with our curated collection of cat-themed treasures — cozy t-shirts, charming cups, and delightful souvenirs. Embrace feline magic and bring a touch of Italiya's charm into your everyday moments. Explore, shop, and immerse yourself in the joy of cats.
+      description: `\nExplore the API documentation for Italiya's Whisker Wonders website.\n\n🌐
+[Visit Italiya's website](https://alexandrbig1.github.io/italiya/)\n\n\n💅
+[Frontend GitHub repository](https://github.com/Alexandrbig1/italiya)\n\n\n🧰 [Backend GitHub repository](https://github.com/Alexandrbig1/italiya-backend)\n\n\n Step into the enchanting world of Italiya: our charismatic orange cat. Indulge in whisker wonders with our curated collection of cat-themed treasures — cozy t-shirts, charming cups, and delightful souvenirs. Embrace feline magic and bring a touch of Italiya's charm into your everyday moments. Explore, shop, and immerse yourself in the joy of cats.
   `,
       version: "1.0.0",
     },
